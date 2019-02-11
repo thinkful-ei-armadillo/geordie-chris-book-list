9c3cfeaca277d851f642172f3ff1607737c7fbe0
Christopher <christopher.carnivale@gmail.com>
2019-02-11 12:47:07 -0500

    grabbed user input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Search from './Search.js'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+
+class App extends Component {
+  
+  state = {
+    searchTerm: null,
+    bookList: [],  
+  }
+
+  userInput = (event) => {
+    event.preventDefault() 
+    const searchValue = event.currentTarget.search.value;  
+    this.setState({
+      searchTerm: searchValue, 
+    })
+}
+  componentDidMount(){
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}&key="AIzaSyBVwHBPcyE5hwVtKaWt-L250MlbWHbgs5Q"`)
+      .then(res => console.log(res)); 
+  }
+  
+  render(){
+    return (
+      <main className='App'>
+        <Search userInput={this.userInput}></Search>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+    render(){
+        //api request with the search term
+        //promise checks for error and parses json
+        //json object, loop over it, create lis
+        return(
+            <form onSubmit={ this.props.userInput }>
+                <label forhtml="search">Search:</label>
+                <input name="search" type="search" id="search" placeholder="Fear and Loathing in Las Vegas"/>
+                <input name="search-submit" type="submit" value="Search"/>
+            </form>
+        )
+    }
+}
+
+export default Search
